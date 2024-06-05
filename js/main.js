15cc057e6bc7a9e3c867c68f2959e70039f6c691
Dontecare <Dontecare1@gmail.com>
2024-06-05 01:14:26 -0500

    Created PWA
    Test

--- js/main.js
+++ js/main.js
@@ -185,3 +185,15 @@ function toggleMenu() {
   const mobileMenu = document.getElementById('mobile-menu');
   mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
 }
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(error) {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+    console.log('Service worker installing...');
+    // Add a call to skipWaiting here if you want to force the waiting service worker to become the active service worker
+    // self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
