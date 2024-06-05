cb66576bbccf7b59c3cfac17a87be1cc4b6cc5b6
Dontecare <Dontecare1@gmail.com>
2024-06-04 23:47:07 -0500

    added fade and slide in effects to home page

--- js/main.js
+++ js/main.js
@@ -143,3 +143,26 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initial check when the page loads
   checkSection();
 });
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.querySelectorAll('.slide-in-left, .slide-in-right, .fade-in, .fade');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);  // Stop observing once the element is visible
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  elements.forEach(element => {
+    observer.observe(element);
+  });
+});
